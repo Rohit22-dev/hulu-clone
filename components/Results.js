1dfc6123c1f6e4fc25dd3e1950f8ed77acdd78e2
Rohit22-dev <rkflash22@gmail.com>
2022-10-14 22:11:30 +0530

    Added local variable

--- components/Results.js
+++ components/Results.js
@@ -5,7 +5,7 @@ const Results = ({ results }) => {
   return (
     <div className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-5 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
-        <Thumbnail key={result.id} {...result} />
+        <Thumbnail key={result.id} result={result} />
       ))}
     </div>
   );
--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,35 +2,27 @@ import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({
-  poster_path,
-  backdrop_path,
-  overview,
-  title,
-  release_date,
-  vote_count,
-  media_type,
-  name,
-  first_air_date,
-},ref) => {
-  const BASE_URL = `https://www.themoviedb.org/t/p/w220_and_h330_face${backdrop_path||poster_path}`;
+const Thumbnail = forwardRef(({result},ref) => {
+  const BASE_URL = `https://www.themoviedb.org/t/p/w220_and_h330_face${result.backdrop_path||result.poster_path}`;
 
   return (
     <div ref={ref} className="p-4 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50" >
       <Image layout="responsive" src={BASE_URL} height={1920} width={1920} className="object-fill rounded-xl"/>
       <div className="p-2">
-        <p className="truncate max-w-md">{overview}</p>
+        <p className="truncate max-w-md">{result.overview}</p>
         <h2 className="mt-1 text-xl sm:text-2xl text-white transition-all duration-100 ease group-hover:font-bold">
-          {title || name}
+          {result.title || result.name}
         </h2>
         <p className="flex items-center opacity-0 group-hover:opacity-100">
-          {media_type && `${media_type} |`} {release_date || first_air_date} |{" "}
+          {result.media_type && `${result.media_type} |`} {result.release_date || result.first_air_date} |{" "}
           <ThumbUpIcon className="h-5 mx-2" />
-          {vote_count}
+          {result.vote_count}
         </p>
       </div>
     </div>
   );
 });
+
+Thumbnail.displayName = 'Thumbnail';
 
 export default Thumbnail;
